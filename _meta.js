59ed777a6fe27f5a27bf54249cce1b482b846cca
RedWilly <thompsonau023@gmail.com>
2025-05-18 15:22:05 +0000

    docs for diffusion studio

---
+++ _meta.js
@@ -0,0 +1,26 @@
+const meta = {
+  index: "",
+  terminology: "Terminology",
+  composition: "Composition",
+  checkpoints: "Checkpoints",
+  layers: "Layers",
+  clips: "Clips",
+  transitions: "Transitions",
+  rendering: "Rendering",
+  'keyframe-animations': "Keyframe Animations",
+  masks: "Masks",
+  timestamp: "Timestamp",
+  events: "Events",
+  transcript: "Transcript",
+  'separator-1': {
+    type: 'separator',
+    title: 'Legacy',
+  },
+  'separator-2': {
+    type: 'separator',
+    title: 'Migration Guides',
+  },
+  "v2-v3": "v2.x to v3.x",
+};
+
+export default meta;
